a5f3b161c274d36a8158f4de63f7048a2783d05a
geofrizz <geofrizz@gmail.com>
2024-08-20 10:54:53 +0200

    fixed some issues for run docker-jupyter !

--- scripts/run_docker_jupyter.sh
+++ scripts/run_docker_jupyter.sh
@@ -5,10 +5,10 @@
 # you need the follow row otherwise comment/delete
 #cd ..
 
-# test - remove the container
-docker run --rm -it --name source_jupyter -v ${PWD}/notebooks:/home/jovyan/notebooks/ -p 8888:8888 docker_source_jupyter:1.4.2
+# remove the container
+docker run --rm -it --name source_jupyter -v ${PWD}/:/home/jovyan/ -p 8888:8888 docker_source_jupyter:1.4.2
 
-# OK
-#docker run -it --name source_jupyter -v ${PWD}/notebooks:/home/jovyan/notebooks/ -p 8888:8888 docker_source_jupyter:1.4.2
+# mantain the container
+#docker run -it --name source_jupyter -v ${PWD}/:/home/jovyan/ -p 8888:8888 docker_source_jupyter:1.4.2
 
 #
